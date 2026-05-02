3c241f2cf7c845913c648b2079181f161f5d9df7
meghanashalivaha <kummarim437@gmail.com>
2026-05-02 22:33:22 +0530

    Implement findPosition method to locate set bit

---
+++ Position of the Set Bit.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findPosition(self, n):
+        # code here 
+        if n == 0:
+            return -1
+    
+        if (n & (n - 1)) != 0:
+            return -1
+        
+        pos = 1
+        while n > 1:
+            n = n >> 1
+            pos += 1
+        
+        return pos
